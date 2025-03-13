5b9df64f2d08ccccc272a3ad2709fb063242d773
lorenzo_uliano <blazeylost@outlook.com>
2025-03-13 20:27:48 -0300

    home

--- resources/js/Pages/Home/Home.jsx
+++ resources/js/Pages/Home/Home.jsx
@@ -3,8 +3,27 @@ import { Head } from "@inertiajs/react"
 
 
 export default function Home() {
-    return <AuthenticatedLayout>
+    return <AuthenticatedLayout
+        header={
+            <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                Home
+            </h2>
+        }    
+    >
         <Head title="Home" />
+
+        <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            <h1>Pagina inicial</h1>
+                            <div>
+                                <p>Teste</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         
     </AuthenticatedLayout>
 }
