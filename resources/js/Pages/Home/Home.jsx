edb62aac32c42aa152cda628e8f7e056d67af281
Aiinoot <antoniope1017@gmail.com>
2025-03-13 21:46:55 -0300

    teste

--- resources/js/Pages/Home/Home.jsx
+++ resources/js/Pages/Home/Home.jsx
@@ -5,6 +5,6 @@ import { Head } from "@inertiajs/react"
 export default function Home() {
     return <AuthenticatedLayout>
         <Head title="Home" />
-        
+        <h1>teste</h1>
     </AuthenticatedLayout>
 }
