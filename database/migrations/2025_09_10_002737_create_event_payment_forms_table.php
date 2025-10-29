0c4458862e24963628c26f2455f378a9d76017c6
lorenzo_uliano <lorenzo.uliano@minhaentrada.com.br>
2025-10-29 18:49:33 -0300

    implementado migrations de formas de pagamento

---
+++ database/migrations/2025_09_10_002737_create_event_payment_forms_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_payment_forms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained();
+            $table->foreignId('payment_form_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_payment_forms');
+    }
+};
